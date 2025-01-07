fd85295e93eff7541913072fd7fd97e694948dca
joaovictordejesus <166874133+joaovictordejesus@users.noreply.github.com>
2025-01-07 15:15:46 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+import java.util.Date;
 
+public class Main {
+    public static void main(String[] args) {
+        // Criando o autor
+        Autor autor = new Autor("Jessica Felix", 35);
+
+        // Criando o livro
+        Livro livro = new Livro("Java for Beginners", autor, "Tecnologia");
+
+        // Criando o usuário
+        Usuario usuario = new Usuario("Lucas Rafael", 25);
+
+        // Criando as datas de retirada e devolução
+        Date dataRetirada = new Date();
+        Date dataDevolucao = new Date();
+
+        // Criando o empréstimo
+        Emprestimo emprestimo = new Emprestimo(livro, usuario, dataRetirada, dataDevolucao);
+
+        // Exibindo os detalhes do empréstimo
+        System.out.println("O livro não está disponível");
+        System.out.println(emprestimo);
+    }
+}
